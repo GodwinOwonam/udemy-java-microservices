23f70a80717264d3f55491f6ef02570b3172fb90
GodwinOwonam <godwin.owonamg5@gmail.com>
2024-06-10 11:02:10 +0530

    Added Service registry module

---
+++ register-services/src/test/java/net/javaguides/registerservices/RegisterServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.registerservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
