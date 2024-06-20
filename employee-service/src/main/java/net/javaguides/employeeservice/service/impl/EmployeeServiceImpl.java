f622658805739f108c5e7bc1e9dbb4192231a423
GodwinOwonam <godwin.owonamg5@gmail.com>
2024-06-21 03:16:01 +0530

    FEAT: Microservices communication with RestTemplate -deprecated

--- employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package net.javaguides.employeeservice.service.impl;
 
 import lombok.AllArgsConstructor;
+import net.javaguides.employeeservice.dto.DepartmentDto;
 import net.javaguides.employeeservice.dto.EmployeeDto;
+import net.javaguides.employeeservice.dto.EmployeeWithDepartmentDto;
 import net.javaguides.employeeservice.dto.EmployeeWithoutIdDto;
 import net.javaguides.employeeservice.entity.Employee;
 import net.javaguides.employeeservice.exceptions.ResourceNotFoundException;
@@ -9,32 +11,63 @@ import net.javaguides.employeeservice.mappers.EmployeeMapper;
 import net.javaguides.employeeservice.repository.EmployeeRepository;
 import net.javaguides.employeeservice.service.EmployeeService;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
     private EmployeeRepository employeeRepository;
 
-    private ModelMapper modelMapper;
+    // private ModelMapper modelMapper;
+    private RestTemplate restTemplate;
 
     @Override
     public EmployeeDto create(EmployeeWithoutIdDto employeeDto) {
         // Employee employee = modelMapper.map(employeeDto, Employee.class); // Using ModelMapper
 
-        Employee employee = EmployeeMapper.MAPPER.toEmployee(employeeDto);
+        Employee employee = new Employee();
+        employee.setFirstName(employeeDto.getFirstName());
+        employee.setLastName(employeeDto.getLastName());
+        employee.setEmail(employeeDto.getEmail());
+        employee.setDepartmentCode(employeeDto.getDepartmentCode());
 
         Employee saved = employeeRepository.save(employee);
 
         // return modelMapper.map(saved, EmployeeDto.class); // Using ModelMapper
-        return EmployeeMapper.MAPPER.toEmployeeDto(saved);
+        return new EmployeeDto(
+            saved.getId(),
+            saved.getFirstName(),
+            saved.getLastName(),
+            saved.getEmail(),
+            saved.getDepartmentCode()
+        );
     }
 
     @Override
-    public EmployeeDto getById(Long employeeId) {
+    public EmployeeWithDepartmentDto getById(Long employeeId) {
         Employee employee = employeeRepository.findById(employeeId).get();
 
         // return modelMapper.map(employee, EmployeeDto.class); // Using Model Mapper
-        return EmployeeMapper.MAPPER.toEmployeeDto(employee);
+        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(
+            "http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+            DepartmentDto.class
+        );
+
+        DepartmentDto departmentDto = responseEntity.getBody();
+
+        EmployeeDto employeeDto = new EmployeeDto(
+            employee.getId(),
+            employee.getFirstName(),
+            employee.getLastName(),
+            employee.getEmail(),
+            employee.getDepartmentCode()
+        );
+
+        return new EmployeeWithDepartmentDto(
+            employeeDto,
+            departmentDto
+        );
     }
 }
